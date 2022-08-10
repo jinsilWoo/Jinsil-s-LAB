ba58232a914ed0701633c3cfb4576af84c581dc9
jinsilWoo <wryt9224@gmail.com>
2022-08-10 12:06:06 +0900

    [FEAT]#1-BottomNavigation 과 ViewPager 연결

--- app/src/main/java/com/example/jinsilslab/activity/MainActivity.kt
+++ app/src/main/java/com/example/jinsilslab/activity/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
 
         initAdapter()
+        initBottomNavigation()
     }
 
     private lateinit var viewPager: ViewPager2
@@ -32,6 +33,42 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         viewPager.adapter = viewPagerAdapter
     }
 
+    private fun initBottomNavigation() {
+        // 슬라이드 해서 페이지가 변경되면 각각 맞는 페이지로 바텀네비게이션 아이콘 활성화
+        binding.vpMain.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bnMain.menu.getItem(position).isChecked = true
+                }
+            }
+        )
+
+        //바텀네비게이션 선택 시 뷰페이저의 currentItem 값도 같이 변경
+        binding.bnMain.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    binding.vpMain.currentItem = 0
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_store -> {
+                    binding.vpMain.currentItem = 1
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_like -> {
+                    binding.vpMain.currentItem = 2
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    binding.vpMain.currentItem = 3
+                    return@setOnItemSelectedListener true
+                }
+
+            }
+        }
+
+    }
 
     override fun onBackPressed() {
         if (viewPager.currentItem == 0) {
