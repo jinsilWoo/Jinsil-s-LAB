08e4a0d60cf90478563c5796bd08a9bafaba6587
jinsilWoo <wryt9224@gmail.com>
2022-09-16 23:21:24 +0900

    [FEAT]#8-DiffUtil 수정

--- app/src/main/java/com/example/jinsilslab/util/DiffUtil.kt
+++ app/src/main/java/com/example/jinsilslab/util/DiffUtil.kt
@@ -3,7 +3,7 @@ package com.example.jinsilslab.util
 import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
 
-class DiffUtilCallback<T:Any>() :
+class DiffUtil<T:Any>() :
     DiffUtil.ItemCallback<T>() {
 
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
